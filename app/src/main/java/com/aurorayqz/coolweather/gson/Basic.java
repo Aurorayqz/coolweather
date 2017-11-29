a870cca07a24fde38344640876afe478cea8e9d1
Aurorayqz <aurorayqz@foxmail.com>
2017-11-30 00:14:28 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/aurorayqz/coolweather/gson/Basic.java
@@ -0,0 +1,21 @@
+package com.aurorayqz.coolweather.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Aurorayqz on 2017/11/29.
+ */
+
+public class Basic {
+    @SerializedName("city")
+    public String cityName;
+
+    @SerializedName("id")
+    public String weatherId;
+
+    public Update update;
+    public class Update{
+        @SerializedName("loc")
+        public String updateTime;
+    }
+}
